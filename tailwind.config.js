1cefda2b3b787fd8c486de2bb4a05835a3918082
Igor Lopes Ribeiro <igor.lop.r@gmail.com>
2023-06-04 13:11:59 -0300

    Repair: Enviei de forma errada, estou arrumando o dark-mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: [
     "./**/*.html",
     "./templates/*.html",
     "./templates/**/*.html",
     "./templates/**/**/*.html",
-    "**/**/*.html"
+    "**/**/*.html",
+    "**/**/*.py",
+    "tabnews/static/js/custom.js"
   ],
   theme: {
     extend: {
       colors: {
           'dark': '#1f2328',
+          'darkMode': '#0d1117',
       }
     },
   },
