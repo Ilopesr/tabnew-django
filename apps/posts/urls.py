348f9ba2f7f96f3c8328220658bd4a470a43594a
Igor Lopes Ribeiro <igor.lop.r@gmail.com>
2023-06-06 18:50:36 -0300

    updated: change scheme for posts

--- apps/posts/urls.py
+++ apps/posts/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from apps.posts import views
 
 urlpatterns = [
-    path('comentar/<int:pk>/<post_slug>/', views.add_post, name="add_post" )
+
 ]
--- apps/posts/views.py
+++ apps/posts/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.views.generic import CreateView,DetailView,TemplateView
-from django.urls import reverse,resolve
+from django.views.generic import CreateView, DetailView, TemplateView
+from django.urls import reverse, resolve
 
 
-from apps.posts.models import  Post, Comment
+from apps.posts.models import Post
 from apps.posts.forms import NewPostForm
 from apps.accounts.models import Account
+
 
 class NewPostView(CreateView):
     template_name = "pages/posts/new_post.html"
@@ -29,39 +30,13 @@ class PostDetailView(DetailView):
         try:
             user_slug = self.kwargs.get('user_slug')
             post_slug = self.kwargs.get('post_slug')
-            queryset = get_object_or_404(Post, user__slug=user_slug, slug=post_slug)
+
+            queryset = get_object_or_404(
+                Post, user__slug=user_slug, slug=post_slug)
+
             context = {
-                'object': queryset
+                'object': queryset,
             }
             return render(self.request, self.template_name, context)
         except ValueError:
             pass
-
-
-
-def add_post(request, pk , post_slug):
-    user = Account.objects.get(email=request.user.email)
-    if 'comment_comment' in  request.POST and pk:
-        description = request.POST['comment_comment']
-        comment = Comment.objects.get(pk=pk)
-        new_comment = Comment.objects.create(
-            user=user,
-            description=description,
-            parent_comment=comment,
-        )
-        new_comment.save()
-        return redirect('post_detail', user_slug=user.slug, post_slug=post_slug)
-    elif 'comment_post' in request.POST and pk:
-        description = request.POST['comment_post']
-        post = Post.objects.get(pk=pk)
-        new_comment = Comment.objects.create(
-            user=user,
-            description=description,
-            post=post,
-        )
-        new_comment.save()
-        post.comments.add(new_comment)
-        return redirect('post_detail', user_slug=user.slug, post_slug=post.slug)
-
-
-
